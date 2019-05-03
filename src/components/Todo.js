f12ca8bfe79553b3d1997f2694dc44acc79d8962
shivam123425 <shivam123425@gmail.com>
2019-05-03 22:29:20 +0530

    Use useReducer hook instead of useState

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
 const Todo = props => {
   const [todoName, setTodoName] = useState("");
-  const [todoList, setTodoList] = useState([]);
+  // const [todoList, setTodoList] = useState([]);
   const [submittedTodo, setSubmittedTodo] = useState(null);
+
+  const todoListReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD":
+        return state.concat(action.payload);
+      case "SET":
+        return action.payload;
+      case "REMOVE":
+        return state.filter(todo => todo.id !== action.payload);
+
+      default:
+        return state;
+    }
+  };
+
+  const [todoList, dispatch] = useReducer(todoListReducer, []);
 
   // useEffect runs after every render cycle by default!
   useEffect(() => {
@@ -17,7 +33,7 @@ const Todo = props => {
         for (const key in todoData) {
           todos.push({ id: key, name: todoData[key].name });
         }
-        setTodoList(todos);
+        dispatch({ type: "SET", payload: todos });
       })
       .catch(err => {
         console.log(err);
@@ -35,7 +51,7 @@ const Todo = props => {
 
   useEffect(() => {
     if (submittedTodo) {
-      setTodoList(todoList.concat(submittedTodo));
+      dispatch({ type: "ADD", payload: submittedTodo });
     }
   }, [submittedTodo]);
 

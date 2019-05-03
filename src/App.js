f29b932633c8968db94e5e091075827cd804b2f0
shivam123425 <shivam123425@gmail.com>
2019-05-03 21:01:28 +0530

    Simulate routing using useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import Todo from "./components/Todo";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Todo />
-      </div>
-    );
-  }
-}
+const App = props => {
+  const [page, setPage] = useState("auth");
+
+  const switchPage = pageName => {
+    setPage(pageName);
+  };
+
+  return (
+    <div className="App">
+      <Header
+        onLoadTodos={switchPage.bind(this, "todos")}
+        onLoadAuth={switchPage.bind(this, "auth")}
+      />
+      <hr />
+      {page === "auth" ? <Auth /> : <Todo />}
+    </div>
+  );
+};
 
 export default App;
